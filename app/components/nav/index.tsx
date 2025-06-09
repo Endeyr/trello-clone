8c2b79984f417aed0fce454312baee4fdb3e9e66
Endeyr <aaron.thompson42594@gmail.com>
2025-06-09 13:17:01 -0400

    fix: notification bell

--- app/components/nav/index.tsx
+++ app/components/nav/index.tsx
@@ -61,7 +61,7 @@ const Nav = () => {
 					{/* Right side - User menu */}
 					<div className="flex items-center space-x-4">
 						{/* Notifications */}
-						<button className="p-2 text-gray-500 hover:text-gray-700 hover:bg-gray-100 rounded-lg transition-colors duration-200">
+						<button className="p-2 text-gray-500 hover:text-gray-700 hover:bg-gray-100 rounded-lg transition-colors duration-200 relative">
 							<svg
 								className="w-5 h-5"
 								fill="none"
@@ -72,9 +72,11 @@ const Nav = () => {
 									strokeLinecap="round"
 									strokeLinejoin="round"
 									strokeWidth={2}
-									d="M15 17h5l-5 5-5-5h5zm-5-10a5 5 0 01-5-5m0 0a5 5 0 015-5m-5 5h10"
+									d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.89 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z"
 								/>
 							</svg>
+							{/* Notification badge */}
+							<span className="absolute -top-1 -right-1 w-3 h-3 bg-red-500 rounded-full"></span>
 						</button>
 
 						{/* User Avatar */}
